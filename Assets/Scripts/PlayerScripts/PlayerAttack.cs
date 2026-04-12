6b263f7bae84e73d47bc88f96cc1fb07ac2c461c
huginho-05 <huginhooo012@gmail.com>
2026-04-12 19:15:50 +0200

    AttackAnimation

--- Assets/Scripts/PlayerScripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerScripts/PlayerAttack.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private Animator animator;
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        animator = GetComponentInChildren<Animator>();
+    }
+    
+    private void OnAttack()
+    {
+        animator.SetTrigger("PlayerAttack");
     }
 }
