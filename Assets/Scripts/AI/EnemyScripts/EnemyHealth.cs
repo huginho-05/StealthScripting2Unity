3647d55147169ac06729755cd15803a87c637daa
huginho-05 <huginhooo012@gmail.com>
2026-04-17 15:35:11 +0200

    EnemyhealthBar

---
+++ Assets/Scripts/AI/EnemyScripts/EnemyHealth.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [Header("Enemy Health")]
+    [SerializeField] private int enemyMaxLife;
+    [SerializeField] private int enemyCurrentLife;
+    
+
+    void Start()
+    {
+        enemyCurrentLife = enemyMaxLife;
+    }
+    
+    public void ReceiveDamage(int damage)
+    {
+        int damageTaken = Mathf.Max(damage, 1);
+        enemyCurrentLife -= damageTaken;
+        if (enemyCurrentLife <= 0)
+        {
+            //Poner que muere
+        }
+    }
+    
+    public void AddHealth(int amount)
+    {
+        enemyCurrentLife += amount;
+        enemyCurrentLife = Mathf.Clamp(enemyCurrentLife, 0, enemyMaxLife);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Enemies"))
+        {
+            ReceiveDamage(10); 
+        }
+    }
+    
+    public int GetCurrentLife()
+    {
+        return enemyCurrentLife;
+    }
+
+    public int GetMaxLife()
+    {
+        return enemyMaxLife;
+    }
+}
