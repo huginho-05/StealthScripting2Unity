bf085b06f9854ffbf2940d08857cfdb6aa05dd2a
huginho-05 <huginhooo012@gmail.com>
2026-04-18 20:39:10 +0200

    AllieSwapsWithPlayer

---
+++ Assets/Scripts/AI/AllieScripts/AllieSwapsWithPlayerAction.cs
@@ -0,0 +1,51 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+using UnityEngine.AI;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "AllieSwapsWithPlayer", story: "[Self] swaps with [Player]", category: "Action", id: "7e6f542fdc19ef7aaaca328691c5e845")]
+public partial class AllieSwapsWithPlayerAction : Action
+{
+    [SerializeReference] public BlackboardVariable<GameObject> Self;
+    [SerializeReference] public BlackboardVariable<GameObject> Player;
+
+    protected override Status OnStart()
+    {
+        if (Self.Value == null || Player.Value == null) return Status.Failure;
+
+        Vector3 oldAlliePos = Self.Value.transform.position;
+        Vector3 oldPlayerPos = Player.Value.transform.position;
+
+        //Mover aliado
+        NavMeshAgent selfAgent = Self.Value.GetComponent<NavMeshAgent>();
+        if (selfAgent != null && selfAgent.enabled)
+            selfAgent.Warp(oldPlayerPos);
+        else
+            Self.Value.transform.position = oldPlayerPos;
+
+        //Mover player
+        CharacterController cc = Player.Value.GetComponent<CharacterController>();
+        Rigidbody rb = Player.Value.GetComponent<Rigidbody>();
+
+        if (cc != null)
+        {
+            //Apagar CharacterController para teletransportar y volverlo a activar al completar el teletransporte
+            cc.enabled = false;
+            Player.Value.transform.position = oldAlliePos;
+            cc.enabled = true;
+        }
+        else if (rb != null)
+        {
+            rb.position = oldAlliePos;
+            rb.linearVelocity = Vector3.zero; //Evitar que se mueva de más por la inercia
+        }
+
+        return Status.Success;
+    }
+
+    protected override Status OnUpdate() => Status.Success;
+}
+
